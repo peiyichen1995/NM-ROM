3d5a38641daff4ee4cc781eb44ff4a3c977fb943
Gary Hu <hugary1995@gmail.com>
2021-11-15 21:22:18 +0800

    Fix projection bug, add gitignore

--- 1d_burger.py
+++ 1d_burger.py
@@ -8,213 +8,210 @@ from solvers import CustomSolver
 from problems import CustomProblem
 from scipy.integrate import solve_ivp
 
-def burgers_time_viscous ( e_num, nu ):
 
-#*****************************************************************************80
-#
-## burgers_time_viscous, 1D time-dependent viscous Burgers equation.
-#
-#  Discussion:
-#
-#    dudt - nu u" + u del u = 0,
-#    -1 < x < 1, 0 < t
-#    u(-1,t) = -1, u(1,t) = 1
-#    u(x,0) = x
-#
-#    This equation is nonlinear in U.
-#
-#  Licensing:
-#
-#    This code is distributed under the GNU LGPL license.
-#
-#  Modified:
-#
-#    21 October 2018
-#
-#  Author:
-#
-#    John Burkardt
-#
-#  Parameters:
-#
-#    Input, integer E_NUM, the number of elements to use.
-#
-#    Input, real NU, the viscosity, which should be positive.
-#    The larger it is, the smoother the solution will be.
-#
+def burgers_time_viscous(e_num, nu):
 
+    # *****************************************************************************80
+    #
+    # burgers_time_viscous, 1D time-dependent viscous Burgers equation.
+    #
+    #  Discussion:
+    #
+    #    dudt - nu u" + u del u = 0,
+    #    -1 < x < 1, 0 < t
+    #    u(-1,t) = -1, u(1,t) = 1
+    #    u(x,0) = x
+    #
+    #    This equation is nonlinear in U.
+    #
+    #  Licensing:
+    #
+    #    This code is distributed under the GNU LGPL license.
+    #
+    #  Modified:
+    #
+    #    21 October 2018
+    #
+    #  Author:
+    #
+    #    John Burkardt
+    #
+    #  Parameters:
+    #
+    #    Input, integer E_NUM, the number of elements to use.
+    #
+    #    Input, real NU, the viscosity, which should be positive.
+    #    The larger it is, the smoother the solution will be.
+    #
 
-  print ( '' )
-  print ( '  Number of elements is %d' % ( e_num ) )
-  print ( '  Viscosity set to %g' % ( nu ) )
+    print('')
+    print('  Number of elements is %d' % (e_num))
+    print('  Viscosity set to %g' % (nu))
 
-  class PeriodicBoundary(SubDomain):
+    class PeriodicBoundary(SubDomain):
 
-    # Left boundary is "target domain" G
-    def inside(self, x, on_boundary):
-      return bool(x[0] < DOLFIN_EPS and x[0] > -DOLFIN_EPS and on_boundary)
+        # Left boundary is "target domain" G
+        def inside(self, x, on_boundary):
+            return bool(x[0] < DOLFIN_EPS and x[0] > -DOLFIN_EPS and on_boundary)
 
-    # Map right boundary (H) to left boundary (G)
-    def map(self, x, y):
-      y[0] = x[0] - 2.0
+        # Map right boundary (H) to left boundary (G)
+        def map(self, x, y):
+            y[0] = x[0] - 2.0
 
 # Create periodic boundary condition
-  pbc = PeriodicBoundary()
+    pbc = PeriodicBoundary()
 #
 #  Create a mesh on the interval [0,+1].
 #
-  x_left = 0.0
-  x_right = +2.0
-  mesh = IntervalMesh ( e_num, x_left, x_right )
+    x_left = 0.0
+    x_right = +2.0
+    mesh = IntervalMesh(e_num, x_left, x_right)
 #
 #  Define the function space to be of Lagrange type
 #  using piecewise linear basis functions.
 #
-  t_num = 1000
-  k = 0
-  t = 0.0
+    t_num = 1000
+    k = 0
+    t = 0.0
 
-  t_plot = 0.0
-  t_final = 0.5
+    t_plot = 0.0
+    t_final = 0.5
 
-  V = FunctionSpace ( mesh, "CG", 1)
-  snapshots = np.zeros((e_num+1, t_num+1))
+    V = FunctionSpace(mesh, "CG", 1)
+    snapshots = np.zeros((e_num + 1, t_num + 1))
 #
 #  Define the boundary conditions.
- # if X <= XLEFT + eps, then U = U_LEFT
- # if X_RIGHT - eps <= X, then U = U_RIGHT
+   # if X <= XLEFT + eps, then U = U_LEFT
+   # if X_RIGHT - eps <= X, then U = U_RIGHT
 
-  u_left = +1.0
-  def on_left ( x, on_boundary ):
-    return ( on_boundary and near ( x[0], x_left ) )
-  bc_left = DirichletBC ( V, u_left, on_left )
+    u_left = +1.0
 
-  u_right = +1.0
-  def on_right ( x, on_boundary ):
-    return ( on_boundary and near ( x[0], x_right ) )
-  bc_right = DirichletBC ( V, u_right, on_right )
+    def on_left(x, on_boundary):
+        return (on_boundary and near(x[0], x_left))
+    bc_left = DirichletBC(V, u_left, on_left)
 
-  bc = [ bc_left, bc_right ]
-  # bc = []
+    u_right = +1.0
 
-  # Sub domain for Periodic boundary condition
+    def on_right(x, on_boundary):
+        return (on_boundary and near(x[0], x_right))
+    bc_right = DirichletBC(V, u_right, on_right)
+
+    bc = [bc_left, bc_right]
+    # bc = []
+
+    # Sub domain for Periodic boundary condition
 
 
 #
 #  Define the initial condition.
 #
-  # u_init = Expression ( "x[0]", degree = 1 )
-  u_init = Expression ( "x[0] < 1 ? 1+A*(sin(2*pi*x[0]-pi/2)+1) : 1", degree = 1, A = nu/2 )
-  # u_init = Expression ( "1+A*(sin(10*pi*x[0]-pi/2)+1)", degree = 1, A = nu/2 )
+    # u_init = Expression ( "x[0]", degree = 1 )
+    u_init = Expression(
+        "x[0] < 1 ? 1+A*(sin(2*pi*x[0]-pi/2)+1) : 1", degree=1, A=nu / 2)
+    # u_init = Expression ( "1+A*(sin(10*pi*x[0]-pi/2)+1)", degree = 1, A = nu/2 )
 
 #
 #  Define the trial functions (u) and test functions (v).
 #
-  u = Function ( V )
-  u_old = Function ( V )
-  v = TestFunction ( V )
+    u = Function(V)
+    u_old = Function(V)
+    v = TestFunction(V)
 #
 #  Set U and U0 by interpolation.
 #
-  u.interpolate ( u_init )
-  u_old.assign ( u )
+    u.interpolate(u_init)
+    u_old.assign(u)
 #
 #  Set the time step.
 #  We need a UFL version "DT" for the function F,
 #  and a Python version "dt" to do a conditional in the time loop.
 #
 
-
-  DT = Constant ( t_final/t_num )
-  dt = t_final/t_num
+    DT = Constant(t_final / t_num)
+    dt = t_final / t_num
 #
 #  Set the source term.
 #
-  f = Expression ( "0.0", degree = 0 )
+    f = Expression("0.0", degree=0)
 #
 #  Write the function to be satisfied.
 #
-  n = FacetNormal ( mesh )
+    n = FacetNormal(mesh)
 #
 #  Write the function F.
 #
-  # F = \
-  # ( \
-  #   dot ( u - u_old, v ) / DT \
-  # + nu * inner ( grad ( u ), grad ( v ) ) \
-  # + inner ( u * u.dx(0), v ) \
-  # - dot ( f, v ) \
-  # ) * dx
+    # F = \
+    # ( \
+    #   dot ( u - u_old, v ) / DT \
+    # + nu * inner ( grad ( u ), grad ( v ) ) \
+    # + inner ( u * u.dx(0), v ) \
+    # - dot ( f, v ) \
+    # ) * dx
 
-  F = \
-  ( \
-    dot ( u - u_old, v ) / DT \
-  + inner ( u * u.dx(0), v ) \
-  - dot ( f, v ) \
-  ) * dx
+    F = \
+        (
+            dot(u - u_old, v) / DT
+            + inner(u * u.dx(0), v)
+            - dot(f, v)
+        ) * dx
 #
 #  Specify the jacobian.
 #
-  J = derivative ( F, u )
+    J = derivative(F, u)
 #
 #  Do the time integration.
 #
 
+    while (True):
 
+        if (k % 25 == 0):
+            # arr = u.vector().get_local()
+            # snapshots[:,k] = arr
+            # plot ( u, title = ( 'burgers time viscous %g' % ( t ) ) )
+            plt.plot(V.tabulate_dof_coordinates(), u.vector().get_local())
+            plt.grid(True)
+            filename = ('output/burgers_time_viscous_%d.png' % (k))
+            plt.savefig(filename)
+            print('Graphics saved as "%s"' % (filename))
+            plt.close()
+            t_plot = t_plot + 0.1
 
+        if (t_final <= t - dt):
+            print('')
+            print('Reached final time.')
+            break
 
+        arr = u.vector().get_local()
+        snapshots[:, k] = arr
 
-  while ( True ):
+        k = k + 1
+        t = t + dt
 
-    if ( k % 25 == 0 ):
-      # arr = u.vector().get_local()
-      # snapshots[:,k] = arr
-      # plot ( u, title = ( 'burgers time viscous %g' % ( t ) ) )
-      plt.plot(V.tabulate_dof_coordinates(), u.vector().get_local())
-      plt.grid ( True )
-      filename = ( 'burgers_time_viscous_%d.png' % ( k ) )
-      plt.savefig ( filename )
-      print ( 'Graphics saved as "%s"' % ( filename ) )
-      plt.close ( )
-      t_plot = t_plot + 0.1
+        # solve ( F == 0, u, bc, J = J )
+        problem = NonlinearVariationalProblem(F, u, bc, J)
+        solver = NonlinearVariationalSolver(problem)
+        solver.solve()
 
-    if ( t_final <= t - dt):
-      print ( '' )
-      print ( 'Reached final time.' )
-      break
+        u_old.assign(u)
 
-    arr = u.vector().get_local()
-    snapshots[:,k] = arr
+    return snapshots
 
-    k = k + 1
-    t = t + dt
-
-    # solve ( F == 0, u, bc, J = J )
-    problem = NonlinearVariationalProblem(F,u,bc,J);
-    solver = NonlinearVariationalSolver(problem);
-    solver.solve();
-
-    u_old.assign ( u )
-
-
-
-  return snapshots
 
 def pod(snapshots, nu, e_num):
     podmodes, svals, _ = spla.svd(snapshots, full_matrices=False)
-    filename = ( 'burgers_viscous_%g.png' % ( nu ) )
+    filename = ('output/burgers_viscous_%g.png' % (nu))
     plt.semilogy(svals, '.')
-    plt.grid ( True )
+    plt.grid(True)
     plt.title('$\mu = $' + str(nu))
-    plt.savefig ( filename )
+    plt.savefig(filename)
 
     # find pod dimension
     err_tol = 1e-5
     poddim = 1
-    err = 1 - np.sum(svals[:poddim])/np.sum(svals)
+    err = 1 - np.sum(svals[:poddim]) / np.sum(svals)
     while (err > err_tol):
         poddim += 1
-        err = 1 - np.sum(svals[:poddim])/np.sum(svals)
+        err = 1 - np.sum(svals[:poddim]) / np.sum(svals)
 
     print('POD dimension: ' + str(poddim))
 
@@ -222,7 +219,7 @@ def pod(snapshots, nu, e_num):
 
     x_left = 0.0
     x_right = +2.0
-    mesh = IntervalMesh ( e_num, x_left, x_right )
+    mesh = IntervalMesh(e_num, x_left, x_right)
 
     t_num = 1000
     k = 0
@@ -233,69 +230,70 @@ def pod(snapshots, nu, e_num):
 
     timegrid = np.linspace(t, t_final, t_num)
 
-    V = FunctionSpace ( mesh, "CG", 1)
+    V = FunctionSpace(mesh, "CG", 1)
     # snapshots = np.zeros((e_num+1, t_num+1))
 
     u_left = +1.0
-    def on_left ( x, on_boundary ):
-      return ( on_boundary and near ( x[0], x_left ) )
-    bc_left = DirichletBC ( V, u_left, on_left )
+
+    def on_left(x, on_boundary):
+        return (on_boundary and near(x[0], x_left))
+    bc_left = DirichletBC(V, u_left, on_left)
 
     u_right = +1.0
-    def on_right ( x, on_boundary ):
-      return ( on_boundary and near ( x[0], x_right ) )
-    bc_right = DirichletBC ( V, u_right, on_right )
 
-    bc = [ bc_left, bc_right ]
+    def on_right(x, on_boundary):
+        return (on_boundary and near(x[0], x_right))
+    bc_right = DirichletBC(V, u_right, on_right)
 
-  #  Define the initial condition.
-    u_init = Expression ( "x[0] < 1 ? 1+A*(sin(2*pi*x[0]-pi/2)+1) : 1", degree = 1, A = nu/2 )
+    bc = [bc_left, bc_right]
 
-  #
-  #  Define the trial functions (u) and test functions (v).
-  #
-    u = Function ( V )
-    u_old = Function ( V )
-    v = TestFunction ( V )
+    #  Define the initial condition.
+    u_init = Expression(
+        "x[0] < 1 ? 1+A*(sin(2*pi*x[0]-pi/2)+1) : 1", degree=1, A=nu / 2)
 
-  #
-  #  Set U and U0 by interpolation.
-  #
-    u.interpolate ( u_init )
-    u_old.assign ( u )
+    #
+    #  Define the trial functions (u) and test functions (v).
+    #
+    u = Function(V)
+    u_old = Function(V)
+    v = TestFunction(V)
 
+    #
+    #  Set U and U0 by interpolation.
+    #
+    u.interpolate(u_init)
+    u_old.assign(u)
 
-  #
-  #  Set the time step.
-  #  We need a UFL version "DT" for the function F,
-  #  and a Python version "dt" to do a conditional in the time loop.
-  #
+    #
+    #  Set the time step.
+    #  We need a UFL version "DT" for the function F,
+    #  and a Python version "dt" to do a conditional in the time loop.
+    #
 
-
-    DT = Constant ( t_final/t_num )
-    dt = t_final/t_num
-  #
-  #  Set the source term.
-  #
-    f = Expression ( "0.0", degree = 0 )
-  #
-  #  Write the function to be satisfied.
-  #
-    n = FacetNormal ( mesh )
-  #
-  #  Write the function F.
-  #
+    DT = Constant(t_final / t_num)
+    dt = t_final / t_num
+    #
+    #  Set the source term.
+    #
+    f = Expression("0.0", degree=0)
+    #
+    #  Write the function to be satisfied.
+    #
+    n = FacetNormal(mesh)
+    #
+    #  Write the function F.
+    #
 
     F = \
-    ( \
-      dot ( u - u_old, v ) / DT \
-    + inner ( u * u.dx(0), v ) \
-    - dot ( f, v ) \
-    ) * dx
-  #
-  #  Specify the jacobian.
-  #
-    J = derivative ( F, u )
+        (
+            dot(u - u_old, v) / DT
+            + inner(u * u.dx(0), v)
+            - dot(f, v)
+        ) * dx
+    #
+    #  Specify the jacobian.
+    #
+    J = derivative(F, u)
 
     J_mat = assemble(J)
     J_array = J_mat.array()
@@ -303,14 +301,12 @@ def pod(snapshots, nu, e_num):
     J_red = np.matmul(J_red, selected_podmodes)
 
     def burgers_nonl_func(ufun):
-        print(ufun)
-        exit()
-        cform = inner(grad(ufun)*ufun, v)*dx
+        cform = inner(ufun.dx(0) * ufun, v) * dx
         cass = assemble(cform)
         return cass
 
-
     # as a vector to vector map
+
     def burgers_nonl_vec(uvec):
         ufun = Function(V)
         ufun.vector().set_local(uvec)
@@ -323,7 +319,6 @@ def pod(snapshots, nu, e_num):
         redconv = selected_podmodes.T.dot(burgers_nonl_vec(inflatedv))
         return -J_red.dot(redvec) - redconv.flatten()
 
-
     u = u.vector().get_local()
     u = np.matmul(selected_podmodes.T, u)
 
@@ -333,61 +328,60 @@ def pod(snapshots, nu, e_num):
     return redburgsol
 
 
-def burgers_time_viscous_test ( ):
+def burgers_time_viscous_test():
 
-#*****************************************************************************80
-#
-## burgers_time_viscous_test tests burgers_time_viscous.
-#
-#  Licensing:
-#
-#    This code is distributed under the GNU LGPL license.
-#
-#  Modified:
-#
-#    21 October 2018
-#
-#  Author:
-#
-#    John Burkardt
-#
-  import time
+    # *****************************************************************************80
+    #
+    # burgers_time_viscous_test tests burgers_time_viscous.
+    #
+    #  Licensing:
+    #
+    #    This code is distributed under the GNU LGPL license.
+    #
+    #  Modified:
+    #
+    #    21 October 2018
+    #
+    #  Author:
+    #
+    #    John Burkardt
+    #
+    import time
 
-  print ( time.ctime ( time.time() ) )
+    print(time.ctime(time.time()))
 #
 #  Report level = only warnings or higher.
 #
-  # level = 30
-  # set_log_level ( level )
+    # level = 30
+    # set_log_level ( level )
 
-  # print ( '' )
-  # print ( 'burgers_time_viscous_test:' )
-  # print ( '  FENICS/Python version' )
-  # print ( '  Solve the time-dependent 1d viscous Burgers equation.' )
+    # print ( '' )
+    # print ( 'burgers_time_viscous_test:' )
+    # print ( '  FENICS/Python version' )
+    # print ( '  Solve the time-dependent 1d viscous Burgers equation.' )
 
-  e_num = 10
-  # nu = 0.05
-  # nus = [1/10**i for i in range(11)]
-  nus = [1.0]
-  for nu in nus:
-      print( 'nu = %g' % ( nu ) )
-      snapshots = burgers_time_viscous ( e_num, nu )
-      # svd
-      solution = pod(snapshots, nu, e_num)
-      print(len(solution))
+    e_num = 10
+    # nu = 0.05
+    # nus = [1/10**i for i in range(11)]
+    nus = [1.0]
+    for nu in nus:
+        print('nu = %g' % (nu))
+        snapshots = burgers_time_viscous(e_num, nu)
+        # svd
+        solution = pod(snapshots, nu, e_num)
+        print(len(solution))
 #
 #  Terminate.
 #
-  # print ( "" )
-  # print ( "burgers_time_viscous_test:" )
-  # print ( "  Normal end of execution." )
-  # print ( '' )
-  # print ( time.ctime ( time.time() ) )
+    # print ( "" )
+    # print ( "burgers_time_viscous_test:" )
+    # print ( "  Normal end of execution." )
+    # print ( '' )
+    # print ( time.ctime ( time.time() ) )
+
+    return
 
 
+if (__name__ == '__main__'):
 
-  return
-
-if ( __name__ == '__main__' ):
-
-  burgers_time_viscous_test ( )
+    burgers_time_viscous_test()
