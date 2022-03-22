d8fc324233d0c6a304bbb8777ce9ed46dce00687
Gary Hu <hugary1995@gmail.com>
2022-03-21 23:48:37 -0400

    Add argument

---
+++ NM_ROM_intrusive/model_definition.py
@@ -0,0 +1,97 @@
+from flax.training import train_state, checkpoints
+from flax.core.frozen_dict import FrozenDict
+import jax.numpy as jnp
+import numpy as np
+
+import jax
+from jax import nn as jnn
+from jax import random
+
+from functools import partial
+
+from flax import linen as nn
+from flax import optim
+
+import optax
+
+from typing import Sequence
+
+from jax.config import config
+config.update("jax_enable_x64", True)
+
+
+class Encoder(nn.Module):
+    latents: Sequence[int]
+    N: int
+    n: int
+
+    @nn.compact
+    def __call__(self, x):
+        x = nn.Dense(self.latents[0], dtype=jnp.float64,
+                     param_dtype=jnp.float64)(x)
+        x = nn.swish(x)
+        xr = nn.Dense(self.n, dtype=jnp.float64, param_dtype=jnp.float64)(x)
+        return xr
+
+
+def gaussian_kernel(window_size, sigma):
+    mu = window_size / 2
+    x = jnp.arange(window_size)
+    window = jnp.exp((-((x-mu)**2))/(2*sigma**2))
+    window = window / jnp.sum(window)
+    return window
+
+@jax.jit
+def dynamic_gaussian_smooth(xs, windows):
+    for i,x in enumerate(xs):
+        xs[i] = jnp.convolve(x, windows[i], mode='same')
+    return jnp.hstack(xs)
+
+
+class Decoder(nn.Module):
+    latents: Sequence[int]
+    N: int
+    n: int
+    n_sigmas: int
+
+    def setup(self):
+        self.sigma = nn.Dense(self.n_sigmas, dtype=jnp.float64,
+                          param_dtype=jnp.float64)
+        self.split_index = np.linspace(0, self.N, self.n_sigmas, endpoint=False, dtype=int)[1:]
+
+    @nn.compact
+    def __call__(self, x):
+        x = nn.Dense(self.latents[0], dtype=jnp.float64,
+                     param_dtype=jnp.float64)(x)
+        x = nn.swish(x)
+        x = nn.Dense(self.N, dtype=jnp.float64, param_dtype=jnp.float64)(x)
+
+        sigmas = self.sigma(x)
+        windows = jax.vmap(gaussian_kernel, in_axes=(None,0))(int(self.N / 20), sigmas)
+        x = dynamic_gaussian_smooth(jnp.split(x, self.split_index), windows)
+
+        return x
+
+
+class VAE(nn.Module):
+    encoder_latents: Sequence[int]
+    decoder_latents: Sequence[int]
+    N: int
+    n: int
+    n_sigmas: int
+
+    def setup(self):
+        self.encoder = Encoder(self.encoder_latents, self.N, self.n)
+        self.decoder = Decoder(self.decoder_latents, self.N, self.n, self.n_sigmas)
+
+    def encode(self, x):
+        return self.encoder(x)
+
+    def decode(self, x):
+        return self.decoder(x)
+
+    def sigmas(self, x):
+        return self.decoder.sigma(x)
+
+    def __call__(self, x):
+        return self.decode(self.encode(x))
---
+++ NM_ROM_intrusive/train.py
@@ -0,0 +1,109 @@
+from flax.training import train_state, checkpoints
+from flax.core.frozen_dict import FrozenDict
+import numpy as np
+
+import h5py
+from fenics import *
+
+import matplotlib.pyplot as plt
+
+import argparse
+
+def read_mesh_and_function(file_name, var_name):
+
+    # Open solution file
+    infile = XDMFFile(file_name + ".xdmf")
+    infile_h5 = h5py.File(file_name + ".h5", "r")
+    t_steps = len(infile_h5[var_name].keys())
+
+    # Read in mesh
+    mesh = Mesh()
+    infile.read(mesh)
+
+    # Read function
+    V = FunctionSpace(mesh, "CG", 1)
+    u = Function(V)
+    solution = np.zeros((V.dim(), t_steps))
+    for i in range(t_steps):
+        infile.read_checkpoint(u, var_name, i - t_steps + 1)
+        solution[:, i] = u.vector().get_local()
+
+    # Clean up
+    infile.close()
+    infile_h5.close()
+
+    return mesh, solution
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('nu', type=float)
+parser.add_argument('n', type=int)
+parser.add_argument('n_sigma', type=int)
+args = parser.parse_args()
+nu = args.nu
+n = args.n
+n_sigmas = args.n_sigma
+
+A = 0.5
+mesh, u_ref = read_mesh_and_function(
+    "../output/burgers_1D/nu_" + str(nu) + "/FOM", "u")
+u_ref = u_ref.T
+
+time_steps, N = u_ref.shape
+u_train = u_ref[np.arange(0, time_steps, 5)]
+n_train = len(u_train)
+M1 = 100
+M2 = 100
+n_epoch = 40000
+
+from model_definition import *
+
+def model():
+    return VAE(encoder_latents=[M1], decoder_latents=[M2], N=N, n=n, n_sigmas=n_sigmas)
+
+
+@jax.jit
+def loss_fn(params, x):
+    xt = jax.vmap(model().apply, in_axes=(None, 0))(params, x)
+    errors = jax.vmap(rel_err, in_axes=(0, 0), out_axes=0)(x, xt)
+    l = jnp.sum(errors**2) / x.shape[0]
+    return l
+
+
+def rel_err(x, xt):
+    return jnp.linalg.norm(x - xt)
+
+
+params = model().init(random.PRNGKey(0), u_train[0])
+tx = optax.adam(0.001)
+opt_state = tx.init(params)
+loss_grad_fn = jax.value_and_grad(loss_fn)
+min_loss = loss_fn(params, u_train)
+best_params = FrozenDict()
+best_params = best_params.copy(params)
+
+CKPT_DIR = "nu_" + str(nu) + "_n_" + str(n) + "_n_sigmas_" + str(n_sigmas)
+
+for i in range(n_epoch):
+    loss_val, grads = loss_grad_fn(params, u_train)
+    if loss_val < min_loss:
+        min_loss = loss_val
+        best_params = best_params.copy(params)
+    updates, opt_state = tx.update(grads, opt_state)
+    params = optax.apply_updates(params, updates)
+    if i % 10 == 0:
+        print('step: {}, loss = {:.6E}, min_loss = {:.6E}'.format(i, loss_val, min_loss))
+    if i % 100 == 0:
+        state = train_state.TrainState.create(apply_fn=model().apply,
+                                            params=params,
+                                            tx=tx)
+        checkpoints.save_checkpoint(
+            ckpt_dir=CKPT_DIR, target=state, step=i, overwrite=True)
+    if loss_val < 1e-6:
+        break
+
+state = train_state.TrainState.create(apply_fn=model().apply,
+                                    params=best_params,
+                                    tx=tx)
+checkpoints.save_checkpoint(
+    ckpt_dir=CKPT_DIR, target=state, step=n_epoch, overwrite=True)
